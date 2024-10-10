0694987f4663493533fa7bbbf5c93c97df12eced
sgwasira <sgwasira@yahoo.com>
2024-10-10 21:43:19 +0200

    added web elements for home page and clicked the customer btn

--- Basics/MainTests.py
+++ Basics/MainTests.py
@@ -1,13 +1,28 @@
 import time
-
-import click
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome()  # This starts the session
+# Initialize the driver
+driver = webdriver.Chrome()  # Ensure that your chromedriver is correctly set up
 driver.maximize_window()
+
+# Open the website
 driver.get("http://www.way2automation.com/angularjs-protractor/banking/#/login")
 
+# Allow the page to load
 time.sleep(5)
 
-driver.find_element(By.XPATH, "//button[@class='btn btn-primary btn-lg'][contains(.,'Customer Login')]").click()
+# WebDriverWait object
+wait = WebDriverWait(driver, 10)
+
+# Page web elements by page
+# Home page
+customerLoginBtn = wait.until(EC.visibility_of_element_located((By.XPATH, "//button[@class='btn btn-primary btn-lg'][contains(.,'Customer Login')]")))
+bankManagerLoginBtn = wait.until(EC.visibility_of_element_located((By.XPATH, "//button[@class='btn btn-primary btn-lg'][contains(.,'Bank Manager Login')]")))
+
+
+customerLoginBtn.click()  # Click the Customer Login button
+
+time.sleep(5)
