f48ebb5e8d60519d8b7031f0d9141eb3462f36a3
sgwasira <sgwasira@yahoo.com>
2024-10-10 22:21:13 +0200

    updated MainTest.py with code for customer login

--- Basics/MainTests.py
+++ Basics/MainTests.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -12,17 +13,31 @@ driver.maximize_window()
 driver.get("http://www.way2automation.com/angularjs-protractor/banking/#/login")
 
 # Allow the page to load
-time.sleep(5)
+time.sleep(2)
 
 # WebDriverWait object
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 5)
 
-# Page web elements by page
-# Home page
-customerLoginBtn = wait.until(EC.visibility_of_element_located((By.XPATH, "//button[@class='btn btn-primary btn-lg'][contains(.,'Customer Login')]")))
-bankManagerLoginBtn = wait.until(EC.visibility_of_element_located((By.XPATH, "//button[@class='btn btn-primary btn-lg'][contains(.,'Bank Manager Login')]")))
+# login page web elements
+customerLoginBtn = wait.until(EC.visibility_of_element_located(
+    (By.XPATH, "//button[@class='btn btn-primary btn-lg'][contains(.,'Customer Login')]")))
+bankManagerLoginBtn = wait.until(EC.visibility_of_element_located(
+    (By.XPATH, "//button[@class='btn btn-primary btn-lg'][contains(.,'Bank Manager Login')]")))
 
-
+# login page actions
 customerLoginBtn.click()  # Click the Customer Login button
+
+# Customer page web elements
+userSelect = wait.until(EC.visibility_of_element_located((By.XPATH, "//select[contains(@id,'userSelect')]")))
+
+# customer page actions
+userSelect_selector = Select(userSelect)
+userSelect_selector.select_by_visible_text("Hermoine Granger")
+
+# Customer page web elements
+customerLoginSelectedUserBtn = wait.until(
+    EC.element_to_be_clickable((By.XPATH, "//button[@class='btn btn-default'][contains(.,'Login')]")))
+customerLoginSelectedUserBtn.click()
+
 
 time.sleep(5)
